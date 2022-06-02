8838f6518a887ae43789b07f39c09a9ac9f4a681
shegzyrey <oluwasegun782@gmail.com>
2022-06-02 16:08:26 -0700

    without formatted printing

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main is the entry point
+ * 
+ * Return sends back zero if 0 or nothing is inputed
+ */
+
+
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
