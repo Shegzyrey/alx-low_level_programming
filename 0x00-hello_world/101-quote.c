47e651235ba77efba72a084a44a146fb98954fe2
shegzyrey <oluwasegun782@gmail.com>
2022-06-02 17:51:20 -0700

    using system calls

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,11 @@
  *Return: Default returns zero if no erros otherwise returns non-zero value.
  */
 int main(void)
-
+{
 
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 70);
 	
 
 	return (1);
+
+}
