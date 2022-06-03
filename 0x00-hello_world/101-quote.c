877f58cee2168a5cfd3c7865d3d8d435be05e2df
shegzyrey <oluwasegun782@gmail.com>
2022-06-02 17:56:45 -0700

    using system calls

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,12 @@
 /**
  *main- Is programs first initialization point
  *
+ *
  *Return: Default returns zero if no erros otherwise returns non-zero value.
  */
 int main(void)
 {
-
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	
-
+	
 	return (1);
-
-}
