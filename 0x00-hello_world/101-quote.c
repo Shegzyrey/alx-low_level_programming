c1206efecde53348590e0aff4f2f5275ddc1dfa0
shegzyrey <oluwasegun782@gmail.com>
2022-06-02 17:48:26 -0700

    using system calls

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,13 @@
-include <unistd.h>
+#include <unistd.h>
 /**
  *main- Is programs first initialization point
  *
  *Return: Default returns zero if no erros otherwise returns non-zero value.
  */
 int main(void)
+
+
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 70);
+	
+
 	return (1);
