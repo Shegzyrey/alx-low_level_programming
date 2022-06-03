b17bfd1fc7cfc2d03a76758458f3b1a2add15378
shegzyrey <oluwasegun782@gmail.com>
2022-06-03 15:14:15 -0700

    list letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *main-entry point for the program.
+ *
+ *Return: returns nothing if zero else error.
+ */
+void main()
+{
+	int ch;
+	for (ch= 'a'; ch<= 'z'; ch++)
+		putchar(ch);
+		putchar ('\n' );
+		getch();
+}
