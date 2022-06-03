40b4c93825f408755a342cb31473f13ff19bfaf2
shegzyrey <oluwasegun782@gmail.com>
2022-06-02 17:32:22 -0700

    using system calls

---
+++ 0x00-hello_world/101-qoute.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ *main- Is programs first initialization point
+ *
+ *Return: Default returns zero if no erros otherwise returns non-zero value.
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n"		'70);
+	return (1);
+}
