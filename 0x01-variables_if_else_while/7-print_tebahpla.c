a9e5b735ce42c5dac615c22bf216d06e1a332102
shegzyrey <oluwasegun782@gmail.com>
2022-06-04 06:32:03 -0700

    reversing the characters

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main-program entry point.
+*Return:0 if no errors, non-zero if errors.
+*/
+
+int main(void)
+{
+	int letter;
+
+	for(letter = 'z'; letter >= 'a'; i--)
+	{
+		printf("%c", letter);
+	}
+	return 0;
+}
