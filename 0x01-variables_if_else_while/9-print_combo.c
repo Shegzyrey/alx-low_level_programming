ce57b62c0431a3ea76733f2075bf344ae80c3ede
shegzyrey <oluwasegun782@gmail.com>
2022-06-04 09:43:08 -0700

    combining integers

---
+++ 0x01-variables_if_else_while/9-print_combo.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *main-entry point for the program.
+ *
+ *
+ *Return: return the possible combinations of units in imtegerd
+ *
+ *
+ */
+int main(void)
+{
+	int number;
+
+	for (number = '0';  number <= '9';  number++)
+	{
+		if (number != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
