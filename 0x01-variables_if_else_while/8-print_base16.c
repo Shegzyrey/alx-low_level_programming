13ffb67adffabb157cfc2e18228620d62e971119
shegzyrey <oluwasegun782@gmail.com>
2022-06-04 07:09:38 -0700

    listing in hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main-program entry point.
+*Return:0 if no errors, non-zero if errors.
+*/
+
+int main(void)
+{
+	int b;
+
+	char hexvalues[] = "0123456789abcdef";
+
+	for (b = 0; b < 16; b++) 
+	{
+		putchar(hexvalues[i]);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
