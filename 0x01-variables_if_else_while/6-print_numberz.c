7ba7fc318fe00fcdbb89d9efcef1d511f41c0baa
shegzyrey <oluwasegun782@gmail.com>
2022-06-04 01:34:22 -0700

    print the first 10 digits of natural number using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *main-entry point for the program.
+ *
+ *
+ *Return: return the list of units in imtegerd
+ *
+ *
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number <= 9; number++)
+	{
+		int putchar(int number);
+	}
+	putchar("\n");
+
+	return (0);
+}
