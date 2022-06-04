781b9e78c2379c99672da54dbd8608d74a7af12e
shegzyrey <oluwasegun782@gmail.com>
2022-06-03 18:32:11 -0700

    print units

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *main-entry point for the program.
+ *
+ *
+ *Return: return the list of units in imtegerd
+ *
+ *
+ */
+int main(void)
+{
+	n = 0;
+
+	for ( n <= '0'; n <= '9'; n++)
+	{
+		printf("%d", n)
+	}
+	printf('\n');
+
+	return (0);
+}	
