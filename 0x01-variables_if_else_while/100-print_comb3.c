e4a1b2cedda496428930345187761afffff5086d
shegzyrey <oluwasegun782@gmail.com>
2022-06-06 21:00:59 -0700

    comb of 9

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,28 +9,29 @@
  */
 int main(void)
 {
-	int number0, number1;
+	int number0, number1, zero = '0';
 
 	for (number0 = '0';  number0 <= '9';  number0++)
 	{
-		for (number1 = '0'; number1 <= '9'; number1++)
+		for (number1 = zero; number1 <= '9'; number1++)
 		{
 			if (number0 < number1)
 			{
 				putchar(number0);
 				putchar(number1);
 			}
-			if (number0 == number1)
+/**			if (number0 == number1)
 			{
 				continue;
 			}
-
+*/
 			if (number0 != '9' || number1 != '9')
 			{
 				putchar(',');
 				putchar(' ');
 			}
 		}
+		zero++;
 	}
 	putchar('\n');
 
