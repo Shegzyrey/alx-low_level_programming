9a45e9c6a3476e28abe7f2d647a5775d0b50a1ab
shegzyrey <oluwasegun782@gmail.com>
2022-06-06 21:39:34 -0700

    turning 9 into 3s

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ *main-entry point for the program.
+ *
+ *
+ *Return: return the possible combinations of units in imtegerd
+ *
+ *
+ */
+int main(void)
+{
+	int number0, number1, number2;
+
+	for (number0 = '0';  number0 <= '9';  number0++)
+	{
+		for (number1 = zero; number1 <= '9'; number1++)
+		{
+			for (number2 = '0'; number2 <= '9', number2++)
+			{
+				if (number0 < number1 && number1 < number2)
+				{
+					putchar(number0);
+					putchar(number1);
+					putchar(number2);
+				}
+				if (number0 = '7' && number1 == '8' && number2 == '9')
+				{
+					break;
+				}	
+				else
+				{
+					putchar(',');
+					putchar(' ');
+			}	
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
