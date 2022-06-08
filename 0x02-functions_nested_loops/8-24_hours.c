aae3fe68ca96727b639fac24a7214826be33448d
shegzyrey <oluwasegun782@gmail.com>
2022-06-07 19:09:43 -0700

    clock

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ *jack_bauer -replicating clock
+ *Description: print every minute
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int h1 = 0, h2 = 0, m1 = 0, m2 = 0;
+
+	while (h1 <= 2)
+	{
+		while (h2 <= 4)
+		{
+			while (m1 <= 5)
+			{
+				while (m2 <= 9)
+				{
+					_putchar(h1);
+					_putchar(h2);
+					_putchar(':');
+					_putchar(m1);
+					_putchar(m2);
+					_putvhar('\n');
+					m2++;
+				}
+				m2++;
+			}
+			m1++;
+		}
+		h2++;
+	}
+	h1++;
+
+}
