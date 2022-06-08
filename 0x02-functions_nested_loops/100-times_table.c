ad8a7ba146a51906790a395e6710579f62f1f952
shegzyrey <oluwasegun782@gmail.com>
2022-06-07 21:30:13 -0700

    tables

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints product values
+ *
+ * @n: number used in multiplication
+ *Descriptio: return table without 15 amd below
+ * Return: void
+ */
+
+void print_times_table(int n)
+{
+	int r = 0, product, c;
+
+	if (n < 0 || n > 15)
+		return (0);
+
+	while (r <= n)
+	{
+		for (c = 0; c <= n; c++)
+		{
+			product = r * c;
+			if (c == 0)
+				_putchar('0' + product);
+			else if (product < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + product);
+			}
+			else
+			{
+				_putchar('0' + product / 100);
+				_putchar('0' + (product - 100) / 100);
+				_putchar('0' + product % 10);
+			}
+			if (c < n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+		r++;
+	}
+}
