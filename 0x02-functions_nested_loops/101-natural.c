cf0323cc6f864a95fc878e9452975f33e31dc891
shegzyrey <oluwasegun782@gmail.com>
2022-06-08 08:46:06 -0700

    getting the multiples of 3&5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio>
+/**
+ * main- function entry point
+ * Description: getting the 3,5 multiples of 1024
+ * Return: 0
+ */
+int main(void)
+{
+	int num, sum = 0;
+
+	while (num <= 1024)
+	{
+		if (num % 3 == 0 || num % 5 == 0)
+		{
+			sum += num;
+		}
+		num++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
