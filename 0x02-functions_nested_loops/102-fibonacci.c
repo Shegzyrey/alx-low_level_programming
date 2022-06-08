6f4bf437ef92ee86fd185a9e13566e2afcc802fb
shegzyrey <oluwasegun782@gmail.com>
2022-06-08 09:00:00 -0700

    fibonacci 1

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main -function entry point
+ * Description: fibonacci sequence
+ * Return: 0
+ */
+int main(void)
+{
+	int fib_count;
+	unsigned long fi1 = 0, fi2 = 1, sum;
+
+	while (fib_count < 50)
+	{
+		sum = fi1 + fi2;
+		fi1 = fi2;
+		fi2 = sum;
+		print("%lu", sum);
+
+		if (fib_count == 49)
+		{
+			printf("\n");
+		}
+		else
+		{
+			printf(", ");
+		}
+		fib_count++;
+	}
+	return (0);
+}
