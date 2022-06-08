f2bdb611d99fe51997fb9d829fe9119b1f2a9abb
shegzyrey <oluwasegun782@gmail.com>
2022-06-07 19:42:03 -0700

    clocks

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,21 +10,21 @@ void jack_bauer(void)
 {
 	int h1, h2, m1, m2, hmax = 9;
 
-	h1 = '0';
-	while (h1 <= '2')
+	h1 = 0;
+	while (h1 <= 2)
 	{
-		if (h1 == '2')
+		if (h1 == 2)
 		{
-			hmax = '3';
+			hmax = 3;
 		}
-		h2 = '0';
+		h2 = 0;
 		while (h2 <= hmax)
 		{
-			m1 = '0';
-			while (m1 <= '5')
+			m1 = 0;
+			while (m1 <= 5)
 			{
-				m2 = '0';
-				while (m2 <= '9')
+				m2 = 0;
+				while (m2 <= 9)
 				{
 					_putchar('0' +  h1);
 					_putchar('0' + h2);
