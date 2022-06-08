a279daf7d05634c7854d36b392eed951e54e7768
shegzyrey <oluwasegun782@gmail.com>
2022-06-07 17:12:25 -0700

    checking absolute values

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_abs - check for absolute value
+ *@n :number to be checked
+ * Return: 0 or 1
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
