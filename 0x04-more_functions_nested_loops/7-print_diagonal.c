9aa468dcec1f58e7af29c2965d9d7d16bd567844
shegzyrey <oluwasegun782@gmail.com>
2022-06-09 19:59:39 -0700

    slashes and diagonals

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_diagonal - use \ to print diagonals
+ * @n: number of n to be printed
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int slas = 0;
+	int spac = 0;
+
+	if (n <= 0)
+	{
+		_putchar('\n')
+	}
+	else
+	{
+		while (slas < n)
+		{
+			while(spac < slas)
+			{
+				_putchar(' ');
+				spac++;
+			}
+			slas++;
+			spac = 0;
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
