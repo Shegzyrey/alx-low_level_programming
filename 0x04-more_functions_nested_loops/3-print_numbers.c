b634007c77f7d7833c42c86ba9abdabd7d63e272
shegzyrey <oluwasegun782@gmail.com>
2022-06-09 17:31:25 -0700

    counting numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -9,11 +9,12 @@
 void print_numbers(void)
 {
 	int i;
-	for (i = '0', i >= '0' && i < '9', i++)
-	{
-		_putchar(i);
 
-	}
+		for (i = '0', i < '9', i++)
+		{
+			_putchar(i);
+
+		}
 
 	_putchar('\n');
 
