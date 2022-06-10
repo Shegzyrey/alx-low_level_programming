0e5d80984716074e12528d040e3014a3819d4989
shegzyrey <oluwasegun782@gmail.com>
2022-06-09 17:24:54 -0700

    counting numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - printing numbers
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	for (i = 0, i >= 0 && i < 9, i++)
+	{
+		_putchar(i);
+
+		_putchar('\n');
+	}
+
+	return (0);
+
+}
