5a9524fe16fe7c5355e4eca14a0b83eacfc2a6ca
shegzyrey <oluwasegun782@gmail.com>
2022-06-14 17:34:05 -0700

    half-rev

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,13 +11,27 @@
 void rev_string(char *s)
 {
 
-	int c, i, j;
+	int c, i, j, len;
 
-	for (i = 0, j = strlen(s) - 1; i < j; i++, j--)
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+
+	}
+
+	j = 0;
+	len = i;
+	i--;
+	while (j < len / 2)
 	{
 		c = s[i];
 		s[i] = s[j];
 		s[j] = c;
+
+		i--;
+		j++;
 	}
 }
 
