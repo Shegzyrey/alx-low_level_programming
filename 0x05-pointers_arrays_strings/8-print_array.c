881810cb41462ebfeaa400f85f22f5ae9663a8e1
shegzyrey <oluwasegun782@gmail.com>
2022-06-14 18:48:19 -0700

    n-elements

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,10 +1,23 @@
 #include "main.h"
 
 /**
- * print_array - eyehe
+ * print_array - print n element of a number of arrays
  *
- * @a: khufkg
- * @n: jhguyhl
- * return: alwats 0
+ * @a: first number
+ * @n: second number
+ * Description: print n element
+ * return: always 0
  */
 
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", *(a + 1));
+		if (i < n - 1)
+			printf(", ");
+	}
+	printf("\n");
+}
