11e09cc626a432e6b762209cac28f71a3ce96be1
shegzyrey <oluwasegun782@gmail.com>
2022-06-16 11:57:23 -0700

    comapring strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcmp -  function compares two string.
+ *
+ * @s1:string 1
+ * @s2: string 2
+ * 
+ * Return: Always 0
+ */
+
+char *_strncpy(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != '\0')
+	{
+		if (s1[i] < s2[i])
+			return (s1[i] - s2[i]);
+		if (s1[i] > s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+
+}
