1758ee3cb6f81edaad39ff10ae438792ec13a43c
shegzyrey <oluwasegun782@gmail.com>
2022-06-16 15:39:53 -0700

    reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reverse_array  - reverse array content
+ *
+ * @a:string to be appended to.
+ * @n: number of elements of array.
+ * Return: always 0.
+ */
+
+void reverse_array(int *a, int n)
+{
+	for (int i = (n - 1); i >= 0; i--)
+	{
+		printf("%d", *(array + 1));
+	}
+	printf("\n");
+	return (0);
+}
