80c564aff24ff8095ef34f8ee26d6e97f333b832
shegzyrey <oluwasegun782@gmail.com>
2022-06-16 20:14:04 -0700

    add 2 strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,15 +15,14 @@ char *_strncat(char *dest, char *src, int n)
 	int i;
 	int j;
 
-	for (i = 0; dest[i] != 0; i++)
+	for (i = 0; dest[i] != '\0'; i++)
+	;
 
-	for (j = 0; src[j] != 0 && j < n; j++)
+	for (j = 0; src[j] != '\0' && j < n; j++)
 	{
-		dest[i] = src[j];
-		i++;
+		dest[i + 1] = src[j];
 	}
-
-	_putchar('\n');
+	dest[i + 1] = '\0';
 
 	return (dest);
 }
