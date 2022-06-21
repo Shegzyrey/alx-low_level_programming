27229441a2e679be0ec0f08904d3ed45f713c63a
shegzyrey <oluwasegun782@gmail.com>
2022-06-21 08:55:40 -0700

    cutting strings

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strspn - returns characters that are not in the order
+ * @s: strings to run through
+ * @accept: modulo string
+ *
+ * Return: the characters left.
+ */
+
+unsigned int _strspn(char *s, char accept)
+{
+	unsigned int i, j, res;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		res = 1;
+		for (j = 0; accpept[i] != '\0'; j++)
+		{
+			if (s[i] == accept[i])
+			{
+				res = 0;
+				break
+			}
+
+		}
+		if (res == 1)
+			break;
+	}
+	return (i)
+}
