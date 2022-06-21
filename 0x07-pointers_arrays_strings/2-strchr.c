1b9d89f09cf197c217ee8fbf0a739e97deacd92c
shegzyrey <oluwasegun782@gmail.com>
2022-06-21 06:51:39 -0700

    finding the character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _strchr - retuns the first occurrence of the character in a string
  * @s: strings to run through
@@ -11,11 +12,11 @@ char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
 	{
-		if (c == *s)
+		if (*s == c)
 		{
 			return (s);
 		}
-		s++;
+		*s++;
 	}
-	return (NULL);
+	return (0);
 }
