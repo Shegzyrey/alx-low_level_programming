2742b03a4fbe3ad272cbe37f36b364dc6ab670a6
shegzyrey <oluwasegun782@gmail.com>
2022-06-22 12:13:15 -0700

    read

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * void - _puts_recursion(char *s)
+ * @s: string to be printed
+ *
+ * Return: s
+ */
+
+void _puts_recursion(char *s)
+{
+	unsigned int i = 0;
+	if (s[i] == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(s[i]);
+	_puts_recursion(s+1);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,2 @@
+0-puts_recursion	function that prints a string
+
