b9e96a034f147864ede0ffe2ec560cc1d9ac32e8
shegzyrey <oluwasegun782@gmail.com>
2022-06-22 22:58:55 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * factorial - print th factorial of numbers
+ * @n: number to factorize
+ *
+ * Return: factorial
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n <= 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
