5d4b030f9e4454dc58837049ce34eed5f58d92be
shegzyrey <oluwasegun782@gmail.com>
2022-06-22 23:29:35 -0700

    power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -5,7 +5,7 @@
  * @x: base number
  * @y: power factor
  *
- * Retunrn: the power of the numbers
+ * Return: the power of the numbers
  */
 
 int _pow_recursion(int x, int y)
@@ -14,6 +14,6 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	else if (y == 0)
 		return (1);
-	else (y > 0)
-		return (x * _pow_recursion(x, y-1);
+
+	return (x * _pow_recursion(x, y - 1));
 }
