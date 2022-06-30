41870d84dc63595ae7af20f9d91f86ceec22ed78
shegzyrey <oluwasegun782@gmail.com>
2022-06-30 05:15:01 -0700

    first exit value

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H_INCLUDED
 
 int _putchar(char c);
-void *mallic_checked(unsigned int b);
+void *malloc_checked(unsigned int b);
 char *string_nconcat(vhar *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
