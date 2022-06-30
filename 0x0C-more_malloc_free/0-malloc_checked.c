9db7bb061ab7a809b982789d24f1a287f78a2d09
shegzyrey <oluwasegun782@gmail.com>
2022-06-30 05:22:15 -0700

    first exit value

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,12 +10,12 @@
 
 void *malloc_checked(unsigned int b)
 {
-	void *c;
+	void *ptr;
 
-	c = malloc(b);
+	ptr = malloc(b);
 
-	if (c == NULL)
+	if (ptr == NULL)
 		exit(98);
 
-	return (c);
+	return(ptr);
 }
