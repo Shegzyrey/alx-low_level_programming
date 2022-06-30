7c13f65a6a0e8eb6ed5d948e7530c8ded77d33b1
shegzyrey <oluwasegun782@gmail.com>
2022-06-30 04:54:06 -0700

    first exit value

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocate memmory using malloc
+ * @b: memory to be allocated
+ *
+ * Return: error cocde if not successful
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *c;
+
+	c = malloc(sizeof(int));
+
+	if (c == NULL)
+		exit(98);
+	return (c);
+}
