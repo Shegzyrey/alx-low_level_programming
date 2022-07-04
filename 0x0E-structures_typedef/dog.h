91af3439c94e2a9140269e930f4be910c77c0939
shegzyrey <oluwasegun782@gmail.com>
2022-07-04 13:12:22 -0700

    structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,15 +3,19 @@
 
 /**
  * struct dog - describing a dog
- *
  * @name: dogs name
  * @age: dogs age
  * @owner: dog owners name.
+ *
+ * Description: this structure describes dogs
  */
+
+_putchar (char *c);
 struct dog
 {
 	char *name;
 	float *age;
 	char *owner;
 };
+
 #endif
