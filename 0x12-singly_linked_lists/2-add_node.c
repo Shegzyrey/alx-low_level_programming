c09839d88596cebaf9f2c4e7795b52e4803d5d3a
shegzyrey <oluwasegun782@gmail.com>
2022-07-15 20:46:44 -0700

    final

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -7,7 +7,7 @@
  * Return: what to return
  */
 
-list_t *add_node(list_t **head, comst char *str)
+list_t *add_node(list_t **head, const char *str)
 {
 	list_t *add;
 	int a;
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -10,7 +10,7 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *add, *current;
-	int s;
+	int a;
 
 	if (head == NULL)
 		return (NULL);
