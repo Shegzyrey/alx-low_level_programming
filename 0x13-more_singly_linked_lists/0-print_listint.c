07f8ae556f9f538bd6a880d8fb4eefb6511567db
shegzyrey <oluwasegun782@gmail.com>
2024-02-06 15:04:24 -0500

    betty issues fix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,9 +1,7 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * print_listint_t - Prints all the elements of a listint_t list.
+ * print_listint - Prints all the elements of a listint_t list.
  * @h: Pointer to the head of the list
  *
  * Return: The number of nodes
@@ -19,6 +17,5 @@ size_t print_listint(const listint_t *h)
 		count_n++;
 		h = h->next;
 	}
-
 	return (count_n);
 }
