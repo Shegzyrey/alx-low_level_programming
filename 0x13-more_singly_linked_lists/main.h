b995f7590ca053ca29afaa2dfed3ee286df29ae5
shegzyrey <oluwasegun782@gmail.com>
2024-02-06 14:44:20 -0500

    updated list and main

--- 0x13-more_singly_linked_lists/main.h
+++ 0x13-more_singly_linked_lists/main.h
@@ -8,25 +8,21 @@
 
 
 /**
- * struct list_s - lincked list structure
- * @str: string to be worked with
- * @len: length of the string
- * @next: next node
- * Description: linked list structure
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Desription: singly linked list node structure
+ *
  */
 
 
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
+	int n;
+	struct listint_s *next;
 } list_t;
 
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
+size_t print_listint_t(const listint_t *h)
 
 #endif
