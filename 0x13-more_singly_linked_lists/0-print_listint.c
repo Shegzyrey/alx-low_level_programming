2fde4bb8515360ecfa02a0d69a2dd91086c3b941
shegzyrey <oluwasegun782@gmail.com>
2024-02-06 14:26:34 -0500

    Printing number of nodes in lists

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint_t - Prints all the elements of a listint_t list.
+ * @h: Pointer to the head of the list
+ *
+ * Return: The number of nodes
+ */
+
+size_t print_listint_t(const listint_t *h)
+{
+	size_t count_n = 0;
+
+	while (h != NUL)
+	{
+		printtf("%d\n", h->n);
+		h = h->next;
+		count_n++
+	}
+
+	return (count_n);
+}
